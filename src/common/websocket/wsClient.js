a218818988bdcaf3a6bc76ca06d16ec5b0555c26
jidan745le <420511176@qq.com>
2025-05-25 00:57:10 +0800

    feat:update

--- src/common/websocket/wsClient.js
+++ src/common/websocket/wsClient.js
@@ -57,7 +57,7 @@ class WebSocketClient {
     });
 
     this.socket.on('error', async error => {
-      this.client.emit('error', error);
+      // this.client.emit('error', error);
       console.error('Socket error:', error, 'error.type', error.type);
       if (error.type === 'unauthorized') {
         console.log('unauthorized 尝试刷新 token');
